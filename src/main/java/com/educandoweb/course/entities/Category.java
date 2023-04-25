69a5d35a63a2445c601967d7ce1b50a826f11952
GlaysonLuiz1 <Glaysonluiz1@gmail.com>
2023-04-25 17:40:54 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,49 +1,30 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
-public class Users implements Serializable {
+@Table(name = "tb_category")
+public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String email;
-    private String phone;
-    private String password;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
-
-    public Users() {
+    public Category() {
 
     }
 
-    public Users(Long id, String name, String email, String phone, String password) {
+    public Category(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
     }
 
     public Long getId() {
@@ -62,30 +43,6 @@ public class Users implements Serializable {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -102,7 +59,7 @@ public class Users implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Users other = (Users) obj;
+        Category other = (Category) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
